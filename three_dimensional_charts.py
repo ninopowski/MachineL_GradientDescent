507220c11956a4a5df0c57d37702ad36816e6ef5
ninopowski <ninoskbd@gmail.com>
2021-09-30 11:08:31 +0200

    Partial derivatives and symbolic computation

--- three_dimensional_charts.py
+++ three_dimensional_charts.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 from matplotlib import cm
+from sympy import symbols, diff
+
 
 
 # Data visualisation with 3D charts
@@ -27,5 +29,19 @@ ax = fig.gca(projection='3d')
 ax.set_xlabel("X", fontsize=20)
 ax.set_ylabel("Y", fontsize=20)
 ax.set_zlabel("f(x, y) - cost", fontsize=20)
-ax.plot_surface(x, y, f(x, y), cmap=cm.coolwarm)
-plt.show()
+ax.plot_surface(x, y, f(x, y), cmap=cm.coolwarm, alpha=0.6)
+#plt.show()
+
+
+# Partial derivatives and symbolic computation
+a, b = symbols("x, y")
+print(f'Our cost function f(x, y) is: {f(a, b)}')
+print(f'Partial derivative wrt x is: {diff(f(a, b), a)}')
+print(f'Value of (f(x, y) at x=1.8 and y=1.0 is: {f(a, b).evalf(subs={a: 1.8, b: 1.0})}')
+
+print(f'Value of the slope wrt x at x=1.8 and y=1.0 is: {diff(f(a, b), a).evalf(subs={a: 1.8, b: 1.0})}')
+
+
+
+
+
