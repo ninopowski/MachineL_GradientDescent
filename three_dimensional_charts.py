77f9bd5df0405980b15c723fa8bd0672a42ae003
ninopowski <ninoskbd@gmail.com>
2021-10-01 11:14:37 +0200

    Batch gradient descent, calc. the p. deriv with functions

--- three_dimensional_charts.py
+++ three_dimensional_charts.py
@@ -3,6 +3,7 @@ import numpy as np
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 from matplotlib import cm
 from sympy import symbols, diff
+from math import log
 
 
 
@@ -16,6 +17,12 @@ from sympy import symbols, diff
 
 def f(x, y):
     return 1/(3**(-x**2-y**2)+1)
+
+def fpx(x, y):
+    return 2*3**(-x**2 - y**2)*x*log(3)/(3**(-x**2 - y**2) + 1)**2
+
+def fpy(x, y):
+    return 2*3**(-x**2 - y**2)*y*log(3)/(3**(-x**2 - y**2) + 1)**2
 
 # Make data
 x = np.linspace(start=-2, stop=2, num=200)
@@ -37,6 +44,7 @@ ax.plot_surface(x, y, f(x, y), cmap=cm.coolwarm, alpha=0.6)
 a, b = symbols("x, y")
 print(f'Our cost function f(x, y) is: {f(a, b)}')
 print(f'Partial derivative wrt x is: {diff(f(a, b), a)}')
+print(f'Partial derivative wrt y is: {diff(f(a, b), b)}')
 print(f'Value of (f(x, y) at x=1.8 and y=1.0 is: {f(a, b).evalf(subs={a: 1.8, b: 1.0})}')
 
 print(f'Value of the slope wrt x at x=1.8 and y=1.0 is: {diff(f(a, b), a).evalf(subs={a: 1.8, b: 1.0})}')
@@ -49,8 +57,8 @@ max_iter = 500
 params = np.array([1.8, 1.0])  # initial guess
 
 for n in range(max_iter):
-    gradient_x = diff(f(a, b), a).evalf(subs={a: params[0], b: params[1]})
-    gradient_y = diff(f(a, b), b).evalf(subs={a: params[0], b: params[1]})
+    gradient_x = fpx(params[0], params[1])
+    gradient_y = fpy(params[0], params[1])
     gradients = np.array([gradient_x, gradient_y])
     params = params - learning_rate * gradients
 
